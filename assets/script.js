c3835d21a1162251b02e69e0e1b8c2d8166de99a
Alexander Carr <alexanderc93@gmail.com>
2022-09-01 10:24:21 -0700

    add arrays of each type of character for password generation

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,23 @@
-// Assignment code here
+// list of lowercase characters to randomly choose from
+let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+// list of uppercase characters to randomly choose from
+let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+// list of number characters to randomly choose from
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// list of symbol characters to randomly choose from
+let symbols = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "/", "]", "^", "_", "`", "{", "|", "}", "~", "]", ";"]; 
+
+// console.log(symbols[Math.floor(Math.random() * symbols.length)])
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
